6d333b2fff57c0abe57b9568b0a378a1d40c1f26
Oluwaseun <50335079+topyo@users.noreply.github.com>
2026-05-19 08:29:14 -0400

    Implement Set-EntraTokenLifetimePolicy function
    Added functionality to set a custom token lifetime policy for an application.

--- module/EntraTokenLifetimes.psm1
+++ module/EntraTokenLifetimes.psm1
@@ -1,12 +1,28 @@
-function Get-EntraTokenLifetimePolicy {
-    Get-MgPolicyTokenLifetimePolicy | Select DisplayName, Definition
-}
-
 function Set-EntraTokenLifetimePolicy {
     param(
         [string]$AppId,
         [timespan]$AccessTokenLifetime = "01:00:00"
     )
-    # Implementation here
+
+    # Connect to Microsoft Graph
+    Connect-MgGraph -Scopes "Policy.ReadWrite.ApplicationConfiguration"
+
+    # Define token lifetime policy
+    $policy = @{
+        "definition" = @(
+            ('{"TokenLifetimePolicy":{"Version":1,"AccessTokenLifetime":"{0}"}}' -f $AccessTokenLifetime)
+        )
+        "displayName" = "CustomTokenLifetimePolicy"
+        "isOrganizationDefault" = $false
+    }
+
+    # Create the policy
+    $newPolicy = New-MgPolicyTokenLifetimePolicy -BodyParameter $policy
+
+    # Assign policy to the specified app
+    Add-MgApplicationTokenLifetimePolicyByRef -ApplicationId $AppId -OdataId "https://graph.microsoft.com/v1.0/policies/tokenLifetimePolicies/$($newPolicy.Id)"
+
+    Write-Host "Token lifetime policy applied successfully to AppId: $AppId"
 }
+
 
